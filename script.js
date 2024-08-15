fc8603f7afabe0d35cc9271738c56f60662ec94f
ingridy16 <106077050+ingridy16@users.noreply.github.com>
2024-08-14 22:42:50 -0300

    aa

--- script.js
+++ script.js
@@ -49,6 +49,28 @@ function mostraAlternativas() {
 
 mostraPergunta();
 
+const perguntas = [
+    {
+    enunciado: "1) Qual é a principal causa do aquecimento global?",
+    alternativas: [
+{
+    texto: "a) Destruição de propriedades",
+    afirmacao: "afirmacao"
+},
+{
+    texto: "b) Uso excessivo de energia solar",
+    afirmacao: "afirmacao"
+},
+{
+    texto: "c) Desmatamento indiscriminado",
+    afirmacao: "afirmacao"
+}
+    ]
+}
+]
+
+
+
 function mostraAlternativas(){
     for(const alternativa of perguntaAtual.alternativas){
             const botaoAlternativas = document.createElement("button");
