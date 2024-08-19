7f7a648b2ae7edb1d7ff1433e6b25310545ef902
ingridy16 <106077050+ingridy16@users.noreply.github.com>
2024-08-18 21:59:07 -0300

    alakakakpa

--- script.js
+++ script.js
@@ -85,7 +85,7 @@ function mostraAlternativas() {
     }
 
     function respostaSelecionada(opcaoSelecionada){
-        const afirmacoes = opcaoSelecionada.afirmacoes;
+        const afirmacoes = opcaoSelecionada.afirmacao;
         historiaFinal += afirmacoes + " ";
             atual++;
             mostraPergunta();
